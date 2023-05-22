477450bf2d1db799795ac773c7ce836a3c997a0f
黄招宇本地 <1007991204@qq.com>
2023-05-22 23:59:11 +0800

    托管服务

---
+++ Ihost/AlberHostService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ihost
+{
+    internal class AlberHostService : IHostedService
+    {
+        private Timer _timer;
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(state =>
+            {
+                Console.WriteLine($"{DateTime.Now.ToString()}");
+            }, null, 0, 1000);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Dispose();
+            return Task.CompletedTask;
+        }
+        public static void Start()
+        {
+            var host = new HostBuilder().ConfigureServices(services =>
+            services.AddHostedService<AlberHostService>()).Build();
+            host.Run();
+        }
+    }
+}
---
+++ IhostWebAPI/Bgservice.cs
@@ -0,0 +1,14 @@
+﻿namespace IhostWebAPI
+{
+    public class Bgservice : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+                Console.WriteLine($"当前时间 {DateTime.Now.ToString()}");
+            }
+        }
+    }
+}
